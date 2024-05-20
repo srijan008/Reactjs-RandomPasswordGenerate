0ca467f54b394c90c89bdc6d363e253a356eb6a4
srijan008 <srijankumar008@gmail.com>
2024-05-20 17:58:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import { LC, NC, SC, UC } from "./data/PassChar";
 function App() {
+  let[upper, Setupper] = useState(false)
+  let[lower, Setlower] = useState(false)
+  let[number, Setnumber] = useState(false)
+  let[symbol, Setsymbol] = useState(false)
+  let[length, Setlength] = useState(10)
+  let[fpass, Setfpass] = useState('')
+
+  const generatPass = () => {
+    let charSet = ''
+    let finalSet = ''
+    if(upper || lower || number || symbol ){
+      if(upper) charSet += UC
+      if(number) charSet += NC
+      if(symbol) charSet += SC
+      if(lower) charSet += LC
+      for (let index = 0; index < length; index++) {
+        finalSet += charSet.charAt(Math.floor(Math.random()*charSet.length))        
+      }
+      Setfpass(finalSet);
+    }
+    else{
+      alert("Check Atleast One Checkbox !!")
+    }
+  }
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(fpass).then(() => {
+      
+    })}
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+        <div className="passwordBox">
+          <h2>Password Generator</h2>
+          
+          <div className="passwordBoxin">
+            <input type="text" value={fpass} readOnly /> <button onClick={copyToClipboard}>Copy</button>
+          </div>
+
+          <div className="passlengh">
+            <label htmlFor="">Password Length</label>
+            <input type="number" max={20} min={10} value={length} onChange={(e) => Setlength(e.target.value)}/>
+          </div>
+          <div className="passlengh">
+            <label htmlFor="">Include Uppercase Letter</label>
+            <input type="checkbox" checked={upper} onChange={ () => Setupper(!upper) }/>
+          </div>
+          <div className="passlengh">
+            <label htmlFor="">Include Lowercase Letter</label>
+            <input type="checkbox" checked={lower} onChange={ () => Setlower(!lower) }/>
+          </div>
+          <div className="passlengh">
+            <label htmlFor="">Include Number</label>
+            <input type="checkbox" checked={number} onChange={ () => Setnumber(!number) }/>
+          </div>
+          <div className="passlengh">
+            <label htmlFor="">Include Symbols</label>
+            <input type="checkbox" checked={symbol} onChange={() => Setsymbol(!symbol)}/>
+          </div>
+          <button className="btn" onClick={generatPass}>Generate Password</button>
+        </div>
     </div>
   );
 }
---
+++ src/data/PassChar.jsx
@@ -0,0 +1,5 @@
+export let SC = "!@#$%^&*()."
+export let UC = "POIUYTREWQASDFGHJKLZXCVBNM"
+export let LC = "poiuytrewqasdfghjklzxcvbnm"
+export let NC = "1234567890"
+
